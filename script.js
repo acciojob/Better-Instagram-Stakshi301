2c7b7d77f5cc5a38593f54539377a42aecaf0f03
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-10-29 21:45:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-
-//     <div class="flex" id="parent">
-//         <div draggable="true" class="container"  ondrop="drop(event)" ondragover="allowDrop(event)">
-//         <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" draggable="true" class="draggable" ondragstart="drag(event)">
-//       </div>
-//       <div draggable="true" class="container"  ondrop="drop(event)" ondragover="allowDrop(event)">
-//         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-C_UAhXq9GfuGO452EEzfbKnh1viQB9EDBQ&s" draggable="true" class="draggable" ondragstart="drag(event)">
-//       </div>
-//       <div draggable="true" class="container"  ondrop="drop(event)" ondragover="allowDrop(event)">
-//         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBD_rOB19y9Uq-TdyNSfoZB8pTdFY5csJF5A&s" draggable="true" class="draggable" ondragstart="drag(event)">
-//       </div>
-//       <div draggable="true" class="container"  ondrop="drop(event)" ondragover="allowDrop(event)">
-//         <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" draggable="true" class="draggable" ondragstart="drag(event)">
-//       </div>
-//       <div draggable="true" class="container"  ondrop="drop(event)" ondragover="allowDrop(event)">
-//         <img src="https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg" draggable="true" class="draggable" ondragstart="drag(event)">
-//       </div>
-//       <div draggable="true" class="container"  ondrop="drop(event)" ondragover="allowDrop(event)">
-//         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBD_rOB19y9Uq-TdyNSfoZB8pTdFY5csJF5A&s" draggable="true" class="draggable" ondragstart="drag(event)">
-//       </div>
-//     </div>
-// </body>
-// <script>
-
-    let draggedImage = null;
+let draggedImage = null;
     function allowDrop(event){
         event.preventDefault();
     }
