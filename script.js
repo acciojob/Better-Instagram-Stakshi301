adcc7bd18b2910c1ecff21b8c43763269cbdb64d
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-10-29 21:37:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
-function drop(event){
-    event.preventDefault();
-    const id = event.dataTransfer.getData('text/plain');
-    const draggable = document.getElementById(id); // image 1
 
-    const targetElement = event.target; // image 2
-    if(targetElement.classList.contains('draggable')){
-        const draggedImage = draggable.style.backgroundImage;
-        const targetImage = targetElement.style.backgroundImage;
+//     <div class="flex" id="parent">
+//         <div draggable="true" class="container"  ondrop="drop(event)" ondragover="allowDrop(event)">
+//         <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" draggable="true" class="draggable" ondragstart="drag(event)">
+//       </div>
+//       <div draggable="true" class="container"  ondrop="drop(event)" ondragover="allowDrop(event)">
+//         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-C_UAhXq9GfuGO452EEzfbKnh1viQB9EDBQ&s" draggable="true" class="draggable" ondragstart="drag(event)">
+//       </div>
+//       <div draggable="true" class="container"  ondrop="drop(event)" ondragover="allowDrop(event)">
+//         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBD_rOB19y9Uq-TdyNSfoZB8pTdFY5csJF5A&s" draggable="true" class="draggable" ondragstart="drag(event)">
+//       </div>
+//       <div draggable="true" class="container"  ondrop="drop(event)" ondragover="allowDrop(event)">
+//         <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" draggable="true" class="draggable" ondragstart="drag(event)">
+//       </div>
+//       <div draggable="true" class="container"  ondrop="drop(event)" ondragover="allowDrop(event)">
+//         <img src="https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg" draggable="true" class="draggable" ondragstart="drag(event)">
+//       </div>
+//       <div draggable="true" class="container"  ondrop="drop(event)" ondragover="allowDrop(event)">
+//         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBD_rOB19y9Uq-TdyNSfoZB8pTdFY5csJF5A&s" draggable="true" class="draggable" ondragstart="drag(event)">
+//       </div>
+//     </div>
+// </body>
+// <script>
 
-        draggable.style.backgroundImage = targetImage;
-        targetElement.style.backgroundImage = draggedImage;
+    let draggedImage = null;
+    function allowDrop(event){
+        event.preventDefault();
     }
-}
+    function drag(event){
+        draggedImage = event.target;
+    }
+
+    function drop(event){
+        event.preventDefault();
+        const targetContainer = event.currentTarget;
+
+       if(targetContainer !== draggedImage.parentElement){
+        const targetImage = targetContainer.querySelector('background-image');
+
+        const tempSrc = targetImage.url;
+        targetImage.url = draggedImage.url;
+        draggedImage.url = tempSrc;
+       }
+    }
