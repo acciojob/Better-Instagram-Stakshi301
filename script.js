1536e0cd5c9d55369cd7cfd7eec15ba6f12e5a88
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-10-31 21:58:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
- let draggedImage = null;
-    function allowDrop(event){
-        event.preventDefault();
-    }
-    function drag(event){
-        draggedImage = event.target;
-    }
+ let draggedImage = null;  // Global variable to hold the dragged image element
 
-    function drop(event){
-        event.preventDefault();
-        const targetContainer = event.currentTarget;
-
-       if(targetContainer !== draggedImage.parentElement){
-        const targetImage = targetContainer.querySelector('img');
-
-        const tempSrc = targetImage.src;
-        targetImage.src = draggedImage.src;
-        draggedImage.src = tempSrc;
-       }
-    }
+  // Function to allow dropping
+  function allowDrop(event) {
+      event.preventDefault(); // Necessary to allow a drop
+  }
+  
+  // Function to handle drag event
+  function drag(event) {
+      draggedImage = event.target; // Set the dragged image to the current element
+  }
+  
+  // Function to handle drop event
+  function drop(event) {
+      event.preventDefault(); // Prevent default behavior
+  
+      const targetContainer = event.currentTarget; // Container where the image is dropped
+  
+      // Ensure that the target container doesn't contain the dragged image already
+      if (targetContainer !== draggedImage.parentElement) {
+          // Get the target image (if any) within the container
+          const targetImage = targetContainer.querySelector('img');
+  
+          // Swap the images if there is a target image
+          if (targetImage) {
+              // Swap the src attribute of the dragged image and target image
+              const tempSrc = targetImage.src;
+              targetImage.src = draggedImage.src;
+              draggedImage.src = tempSrc;
+          } else {
+              // If the target container is empty, move the dragged image to the new container
+              targetContainer.appendChild(draggedImage);
+          }
+      }
+  }
